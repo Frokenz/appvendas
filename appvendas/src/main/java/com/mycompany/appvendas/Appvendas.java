c8fcbc77c2a56ab4f56a87369ef8fb0bf4cebdd1
Erick Rodrigues <115048299+Frokenz@users.noreply.github.com>
2025-07-28 10:56:32 -0300

    criação do projeto
    apenas a criação a do projeto e adição da tela de jframe.

---
+++ appvendas/src/main/java/com/mycompany/appvendas/Appvendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appvendas;
+
+/**
+ *
+ * @author SUPORTE-TI
+ */
+public class Appvendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
